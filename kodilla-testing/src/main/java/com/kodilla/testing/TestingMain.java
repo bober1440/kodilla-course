70225a52c4b09c781feed27fb8286c3edb75567e
bober1440 <mbobrowicz2@gmail.com>
2024-07-10 15:55:04 +0200

    6.1 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,5 +3,20 @@ package com.kodilla.testing;
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6.Wprowadzenie do testowania oprogramowania");
+        String str = "Abrakadabra! EoBuDEiYy";
+        System.out.println(znajdzSamogloski(str));
+    }
+
+    public static  String znajdzSamogloski(String str){
+        String samogloski = "AaEeIiOoUuYy";
+        String result = "";
+        char[] charArray = str.toCharArray();
+
+        for (int i= 0 ; i < charArray.length; i++){
+            if (samogloski.contains((Character.toString(charArray[i])))){
+                result += (charArray[i]);
+            }
+        }
+        return result;
     }
 }
