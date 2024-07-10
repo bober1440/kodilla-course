f62e129746326238de2b70dbfb322d7d0f6c7ea4
bober1440 <mbobrowicz2@gmail.com>
2024-07-10 16:44:40 +0200

    6.2 Added first unit test - calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,22 +1,36 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6.Wprowadzenie do testowania oprogramowania");
-        String str = "Abrakadabra! EoBuDEiYy";
-        System.out.println(znajdzSamogloski(str));
-    }
 
-    public static  String znajdzSamogloski(String str){
-        String samogloski = "AaEeIiOoUuYy";
-        String result = "";
-        char[] charArray = str.toCharArray();
-
-        for (int i= 0 ; i < charArray.length; i++){
-            if (samogloski.contains((Character.toString(charArray[i])))){
-                result += (charArray[i]);
-            }
+        //An Example from the module 6.2
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
         }
-        return result;
+        //First unit test - Calculator
+
+        System.out.println("\nFirst unit test - Calculator");
+        Calculator calculator = new Calculator();
+        calculator.add(2, 2);
+        calculator.subtract(5, 4);
+
+        if (calculator.add(2, 2) == 2 + 2) {
+            System.out.println("Test OK");
+        }  else {
+            System.out.println("Error!");
+        }
+
+        if (calculator.subtract(5, 4) == 5-4) {
+            System.out.println("Test OK");
+        } else{
+            System.out.println("Error!");
+        }
     }
 }
