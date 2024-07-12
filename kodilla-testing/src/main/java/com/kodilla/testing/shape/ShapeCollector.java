1b8e0fcf30af916a711fd333111f725e5e53aeab
bober1440 <mbobrowicz2@gmail.com>
2024-07-12 20:47:34 +0200

    6.4 Added ShapeCollector Test Suite without methods

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeCollector {
+    List<Shape> shapes = new ArrayList<>();
+
+    public void addFigure(Shape shape) {
+        return;
+    }
+    public boolean removeFigure(Shape shape) {
+        return false;
+    }
+    public Shape getFigure(int n){
+        return null;
+    }
+    public String showFigures(){
+        return null;
+    }
+
+
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,83 @@
+package com.kodilla.testing.shape;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+@DisplayName("TDD :ShapeCollector Test Suite")
+public class ShapeCollectorTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests");
+    }
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("This is the end of tests");
+    }
+    @BeforeEach
+    public void beforeEachTest() {
+        testCounter++;
+        System.out.println("Preparing test " + testCounter);
+    }
+    @AfterEach
+    public void afterEachTest() {
+        System.out.println("Finished test " + testCounter);
+    }
+    @org.junit.jupiter.api.Nested
+    @DisplayName("Adding/Removing figures")
+    class AddingRemoveTesting {
+        @Test
+        void addFigureTest(){
+            //Given
+            Shape Square = new Square(4);
+            ShapeCollector shapeCollector = new ShapeCollector();
+            //When
+            shapeCollector.addFigure(Square);
+            //Then
+            assertEquals(1 , shapeCollector.shapes.size());
+        }
+        @Test
+        void removeFigureTest(){
+            //Given
+            Shape Circle = new Circle(5);
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(Circle);
+            //When
+            boolean result = shapeCollector.removeFigure(Circle);
+            //Then
+            assertTrue(result);
+            assertEquals(0 , shapeCollector.shapes.size());
+        }
+    }
+
+    @org.junit.jupiter.api.Nested
+    @DisplayName("Getting/Showing figures")
+    class GettingShowingTesting {
+        @Test
+        void getFigureTest(){
+            //Given
+            Shape Triangle = new Triangle(8,4);
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(Triangle);
+            //When
+            Shape retrievedShape = shapeCollector.getFigure(0);
+            //Then
+            assertEquals(Triangle, retrievedShape);
+        }
+        void showFiguresTest(){
+            //Given
+            Shape Triangle = new Triangle(8,4);
+            Shape Square = new Square(5);
+            Shape Circle = new Circle(5);
+            ShapeCollector shapeCollector = new ShapeCollector();
+            shapeCollector.addFigure(Triangle);
+            shapeCollector.addFigure(Square);
+            shapeCollector.addFigure(Circle);
+            //When
+            String result = shapeCollector.showFigures();
+            //Then
+            assertEquals("Triangle, Square, Circle", result);
+        }
+    }
+}
