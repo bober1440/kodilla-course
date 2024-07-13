ea4da47d5973af34062b3c75b0decacf87b641aa
bober1440 <mbobrowicz2@gmail.com>
2024-07-13 11:42:45 +0200

    6.5 Adding Mockito Libraries

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.weather.stub;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WeatherForecastTestSuite {
+
+    @Test
+    void testCalculateForecastWithStub(){
+        //Given
+        Temperatures temperatures = new TemperaturesStub();
+        WeatherForecast weatherForecast = new WeatherForecast(temperatures);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        assertEquals(5, quantityOfSensors);
+    }
+}
